c2b54c3c60e5cf83fe1894dc7dfce5edf4a4bdea
nelyflores <nely.afloresr@gmail.com>
2017-08-08 18:44:15 -0500

    elimina contactos de contactos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
       $('.parallax').parallax();
        $('.tooltipped').tooltip({delay: 50});
+        $('.modal').modal();
+        $("a.external").click(function() {
+     url = $(this).attr("href");
+     window.open(url, '_blank');
+     return false;
+  });
     });
